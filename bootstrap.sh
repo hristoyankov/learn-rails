51404dfaae51bfee3b01f3fd49009ed6cd98fa50
Hristo Yankov <ico.yankov@gmail.com>
2015-02-01 21:06:46 +0200

    Update bootstrap.sh: provision latest version of rvm.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+# Install GIT
 yum -y install git
+
+# Install RVM
+gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable
