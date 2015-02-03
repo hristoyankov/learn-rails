f61f2424da03fea57f0799ae0cb8e8667d984da7
Hristo Yankov <ico.yankov@gmail.com>
2015-02-03 12:01:57 +0200

    Update bootstrap.sh: provision latest ruby 2.2.0 and rails 4.2.0

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-# Install GIT
-yum -y install git
+# Install GIT, build-essential
+yum update
+yum -y install git curl
 
-# Install RVM
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable
+# Install RVM, Ruby and Rails
+su - vagrant -c "gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3"
+su - vagrant -c "curl -sSL https://get.rvm.io | bash -s stable"
+su - vagrant -c "rvm install ruby-2.2.0"
+su - vagrant -c "rvm use ruby-2.2.0@learn-rails --create"
+su - vagrant -c "gem install rails"
