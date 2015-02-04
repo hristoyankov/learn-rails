8f2303687c0b5bab7e4cbc346161589b59f1c7a9
Hristo Yankov <ico.yankov@gmail.com>
2015-02-04 14:06:19 +0200

    Update bootstrap.sh: ruby-2.2.0@learn-rails is the default gemset. Rails 4.2 is installed into @learn-rails.

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,5 +13,5 @@ yum -y install nodejs npm
 su - vagrant -c "gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3"
 su - vagrant -c "curl -sSL https://get.rvm.io | bash -s stable"
 su - vagrant -c "rvm install ruby-2.2.0"
-su - vagrant -c "rvm use ruby-2.2.0@learn-rails --create"
-su - vagrant -c "gem install rails"
+su - vagrant -c "rvm use ruby-2.2.0@learn-rails --create --ruby-version --default"
+su - vagrant -c "gem install rails -v 4.2.0"
